b86196eb52d95acbbe2e29b30aa7916ed750796c
JinJeon <nanyong01@gmail.com>
2022-05-10 15:22:22 +0900

    feat : Set inputMoney tag to allow number

--- src/Components/Home/VendingMachine/Contorllers/InputMoney.jsx
+++ src/Components/Home/VendingMachine/Contorllers/InputMoney.jsx
@@ -1,12 +1,44 @@
+import { useState } from 'react';
 import styled from 'styled-components';
 
 const InputMoneyDiv = styled.div`
 	padding: 20px;
 	font-size: 20px;
+	display: flex;
+	align-items: center;
+`;
+
+const InputMoneyValue = styled.input`
+	text-align: right;
+	outline: none;
+	border: none;
+	font-size: 20px;
 `;
 
 const InputMoney = () => {
-	return <InputMoneyDiv>0</InputMoneyDiv>;
+	const [money, setMoney] = useState(0);
+
+	const handleFocus = ({ target: { value } }) => {
+		if (value === '0') setMoney('');
+	};
+
+	const handleInput = ({ target: { value } }) => {
+		const rNumber = /[0-9]+$|^$/;
+		if (rNumber.test(value)) setMoney(value);
+	};
+
+	return (
+		<InputMoneyDiv>
+			<InputMoneyValue
+				type="text"
+				placeholder="0"
+				onInput={handleInput}
+				onFocus={handleFocus}
+				value={money}
+			/>
+			원
+		</InputMoneyDiv>
+	);
 };
 
 export default InputMoney;
