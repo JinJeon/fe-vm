470a8b6e0ea866dd99ad074b7068dbdca6904c1f
JinJeon <nanyong01@gmail.com>
2022-05-10 12:31:50 +0900

    feat : Install msw, create mockdata

--- src/Components/Home/VendingMachine/Items/Items.jsx
+++ src/Components/Home/VendingMachine/Items/Items.jsx
@@ -1,11 +1,35 @@
-import styled from 'styled-components';
-
-const ItemsDiv = styled.div`
-	flex: 1 1 0;
-`;
+import itemsApi from 'Service/itemsApi';
+import { useEffect, useState } from 'react';
+import { ItemsDiv, ItemDiv, ItemNameDiv, ItmePriceDiv } from './Items.styled';
 
 const Items = () => {
-	return <ItemsDiv>아이템 위치</ItemsDiv>;
+	const [items, setItems] = useState([]);
+
+	const fetchItems = async () => {
+		const itemsData = await itemsApi.getItems();
+		setItems(itemsData.data);
+	};
+
+	const getList = (array) => {
+		const list = array.map((item) => (
+			<ItemDiv key={item.id}>
+				<ItemNameDiv>{item.name}</ItemNameDiv>
+				<ItmePriceDiv>{item.price}</ItmePriceDiv>
+			</ItemDiv>
+		));
+
+		if (list.length % 2) list.push(<ItemDiv key={array.length} empty={true} />);
+
+		return list;
+	};
+
+	const list = items.length ? getList(items) : null;
+
+	useEffect(() => {
+		fetchItems();
+	}, []);
+
+	return <ItemsDiv>{list}</ItemsDiv>;
 };
 
 export default Items;
