15a3b789a55b8a55d0e47fae33547d33f1955aaa
JinJeon <nanyong01@gmail.com>
2022-05-11 15:57:12 +0900

    feat : Set input money part calculate money in wallet

--- src/Components/Home/VendingMachine/Contorllers/InputMoney/InputMoney.jsx
+++ src/Components/Home/VendingMachine/Contorllers/InputMoney/InputMoney.jsx
@@ -1,19 +1,24 @@
 import { useState, useContext } from 'react';
 
 import CoinsContext from 'Components/Home/CoinsContext';
-import { getPriceType, useDebounce } from 'Util/util';
+import {
+	getPriceType,
+	useDebounce,
+	spendMoney,
+	withdrawMoney,
+} from 'Util/util';
 import { InputMoneyDiv, InputMoneyValue } from './InputMoney.styled';
 
 const InputMoney = () => {
 	const unit = '원';
-	const debounceTime = 500;
+	const debounceTime = 2000;
 
-	const { coinsSum } = useContext(CoinsContext);
-	const [showedMoney, setShowedMoney] = useState('');
-	const [money, setMoney] = useState('');
+	const { coins, coinsSum } = useContext(CoinsContext);
+	const [showedMoney, setShowedMoney] = useState(0);
+	const [money, setMoney] = useState(0);
 
 	const handleFocus = ({ target: { value } }) => {
-		if (value === '0') setShowedMoney('');
+		if (value === '0') setShowedMoney(0);
 	};
 
 	const handleInput = ({ target: { value } }) => {
@@ -24,8 +29,21 @@ const InputMoney = () => {
 	};
 
 	const checkShowedMoney = () => {
-		const isMoneyInWallet = coinsSum < showedMoney - money;
-		return isMoneyInWallet ? setShowedMoney(money) : setMoney(showedMoney);
+		const difference = showedMoney - money;
+		const isMoneyInWallet = coinsSum > difference;
+
+		if (isMoneyInWallet) {
+			const calculatedMoney =
+				difference >= 0
+					? spendMoney(coins, difference)
+					: withdrawMoney(coins, difference);
+			const totalMoney = money + calculatedMoney;
+
+			setMoney(totalMoney);
+			setShowedMoney(totalMoney);
+		} else {
+			setShowedMoney(money);
+		}
 	};
 
 	useDebounce(checkShowedMoney, debounceTime);
--- src/Util/util.js
+++ src/Util/util.js
@@ -19,4 +19,61 @@ const useDebounce = (func, delay) => {
 	}, [callback, delay]);
 };
 
-export { getPriceType, useDebounce };
+const spendMoney = (coins, difference) => {
+	const coinsInWallet = [...coins]; // 복사 방식 변경 필요!
+	let totalCount = coinsInWallet.reduce((pre, post) => {
+		return pre + post.count;
+	}, 0);
+
+	const getTargetMoneyId = (array, targetId = array.length - 1) => {
+		const id = array[targetId].count
+			? targetId
+			: getTargetMoneyId(array, targetId - 1);
+		return id;
+	};
+
+	let targetMoneyId = getTargetMoneyId(coinsInWallet);
+	let remainedPrice = difference;
+
+	while (remainedPrice > 0 && totalCount && targetMoneyId >= 0) {
+		const targetMoney = coinsInWallet[targetMoneyId];
+		const targetPrice = targetMoney.price;
+
+		if (remainedPrice >= targetPrice) {
+			remainedPrice -= targetPrice;
+			targetMoney.count -= 1;
+			totalCount -= 1;
+		} else {
+			targetMoneyId -= 1;
+		}
+
+		if (!targetMoney.count) {
+			targetMoneyId = getTargetMoneyId(coinsInWallet, targetMoneyId - 1);
+		}
+	}
+
+	return difference - remainedPrice;
+};
+
+const withdrawMoney = (coins, difference) => {
+	const coinsInWallet = [...coins]; // 복사 방식 변경 필요!
+
+	let targetMoneyId = coinsInWallet[coinsInWallet.length - 1].id;
+	let remainedPrice = Math.abs(difference);
+
+	while (remainedPrice > 0 && targetMoneyId >= 0) {
+		const targetMoney = coinsInWallet[targetMoneyId];
+		const targetPrice = targetMoney.price;
+
+		if (remainedPrice >= targetPrice) {
+			remainedPrice -= targetPrice;
+			targetMoney.count += 1;
+		} else {
+			targetMoneyId -= 1;
+		}
+	}
+
+	return difference - remainedPrice;
+};
+
+export { getPriceType, useDebounce, spendMoney, withdrawMoney };
